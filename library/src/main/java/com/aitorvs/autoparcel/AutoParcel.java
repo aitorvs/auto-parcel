e4bab3f468cfd4d7ec7cd6884dfb39fbc72ec99d
Aitor Viana <aitorvs@gmail.com>
2016-07-14 14:08:02 +0200

    docs: comments

--- library/src/main/java/com/aitorvs/autoparcel/AutoParcel.java
+++ library/src/main/java/com/aitorvs/autoparcel/AutoParcel.java
@@ -5,6 +5,15 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * An annotation to indicate the auto-parcel that the annotated class needs to be Parcelable
+ *
+ * <pre>
+ * <code>
+ * {@literal @}AutoParcel public abstract class Foo  {...}
+ * </code>
+ * </pre>
+ */
 @Target(ElementType.TYPE) // on class level
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AutoParcel {
